541c942b2a80e74194e21e1f07bed60760885d74
Endriw Costa <endriw.costa3@gmail.com>
2024-08-15 18:37:36 -0300

    Class dados Temporada e epis

--- screenmatch/src/main/java/br/com/alura/screenmatch/service/IConverteDados.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/service/IConverteDados.java
@@ -3,6 +3,6 @@ package br.com.alura.screenmatch.service;
 public interface IConverteDados{
     <T> T obterDados (String json, Class<T> classe);
 
-    // No java, Como não sabemos qual será o retorno, no início dessa mesma linha escrevemos <T> T
+    // No java, Como não sabemos qual será o retorno, usamos o Generics no início dessa mesma linha escrevemos <T> T
     //Usamos o <T> T justamente quado não sabemos qual entidade será devolvida.
 }
